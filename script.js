1c5d52a3d13c33442bccd9e87ad17467d57c161f
Viktor <vityaschel@gmail.com>
2020-11-02 21:32:36 +0400

    So bad yet so genius

--- script.js
+++ script.js
@@ -8,8 +8,11 @@ let allow_any_chars = false;
 let x = 0; let y = 0;
 let render_x = 0; let render_y = 0;
 let marker_x = 0; let marker_y = 0;
+let multiplier = 1;
 let char_width = 12;
 let char_height = 20;
+let font_size = 20;
+let hard_offset_y = 10;
 
 let text = "Hello, world!\nThis is probably\nthe stupidiest\n  the most useless\n    thing\nI'VE\n  EVER\n    MADE\n\nEnjoy!";
 let current_line_length = 7;
@@ -61,6 +64,7 @@ function remove_line(){
   let new_text = ""
   lines.forEach(char => new_text+=char)
   text = new_text
+  marker_move_window(0,-1,1)
   redraw()
   redraw_marker()
 }
@@ -68,27 +72,27 @@ function remove_line(){
 let line_length = [];
 let marker_offset_x = 0;
 function redraw_text(){
-  ctx.font = "20px CourCyr";
+  ctx.font = (font_size*multiplier)+"px CourCyr";
   y = -render_y;
   line_length = []
   y_pseudo_lines_offset = 0
   rendering_line_index = 0
   text.split("\n").forEach(line => {
     let chars_in_line = line.length;
-    let line_width = chars_in_line*char_width;
+    let line_width = chars_in_line*(char_width*multiplier);
     let canvas_width = canvas.getBoundingClientRect().width;
     if(line_width <= canvas_width || line_position != rendering_line_index){
-      ctx.fillText(line, x, y+20);
+      ctx.fillText(line, x, y+hard_offset_y+(10*multiplier));
     } else {
-      let max_chars_in_line = Math.floor(canvas_width / char_width)
+      let max_chars_in_line = Math.floor(canvas_width / (char_width*multiplier))
       let screen_x = Math.floor(char_position / max_chars_in_line)
       let sub_start = screen_x*max_chars_in_line
-      marker_offset_x = screen_x*max_chars_in_line*char_width;
+      marker_offset_x = screen_x*max_chars_in_line*(char_width*multiplier);
       let sub_end = (screen_x+1)*max_chars_in_line
-      ctx.fillText(line.substring(sub_start, sub_end), x, y+20);
+      ctx.fillText(line.substring(sub_start, sub_end), x, y+hard_offset_y+(10*multiplier));
     }
     line_length.push(chars_in_line)
-    y += char_height;
+    y += (char_height*multiplier);
     rendering_line_index += 1
   });
 }
@@ -121,7 +125,7 @@ document.addEventListener("keydown", e => {
       lines_count += 1
       char_position = 0;
       line_position += 1;
-      marker_move_window(0, 1)
+      marker_move_window(0, 1, 0)
       marker_drawings();
       break;
 
@@ -156,7 +160,7 @@ document.addEventListener("keydown", e => {
         return;
         // special keys
       }
-      if(!/^[a-zA-Z0-9а-яА-Я!@#$%^&*\(\)?{}<>'\\\[\]"№%:,.;_+\-= ]{1}$/.test(key) && !allow_any_chars){
+      if(!/^[a-zA-Z0-9а-яА-Я!@#$%^&*\(\)?{}<>'\\\`~[\]"№%:,.;_+\-= ]{1}$/.test(key) && !allow_any_chars){
         key = "?"
       }
       insert_letter(key);
@@ -166,9 +170,8 @@ document.addEventListener("keydown", e => {
   }
 });
 
-let width_block = document.getElementById("font-loaded").getBoundingClientRect().width;
 let fontLoaded = setInterval(() => {
-  if(document.getElementById("font-loaded").getBoundingClientRect().width != width_block){
+  if(document.getElementById("font-loaded").getBoundingClientRect().width != document.getElementById("font-not-loaded").getBoundingClientRect().width){
     clearInterval(fontLoaded);
     marker_move(0,0);
   }
@@ -176,6 +179,12 @@ let fontLoaded = setInterval(() => {
 
 function marker_move(by_x, by_y){
   current_line_length = line_length[line_position+by_y]
+  if(by_y != 0){
+    let canvas_width = canvas.getBoundingClientRect().width;
+    let max_chars_in_line = Math.floor(canvas_width / (char_width*multiplier))
+    let screens = Math.floor(current_line_length/max_chars_in_line)
+    marker_offset_x = screens*max_chars_in_line*char_width
+  }
   if(char_position+by_x > current_line_length){
     if(line_position >= lines_count || by_y != 0) {
       char_position = line_length[line_position+by_y] - (by_x > 0 ? 1 : 0)
@@ -204,38 +213,43 @@ function marker_move(by_x, by_y){
   }
   char_position += by_x;
   line_position += by_y;
-  marker_move_window(by_x, by_y);
+  marker_move_window(by_x, by_y, 0);
   marker_drawings();
 }
 
-function marker_move_window(by_x, by_y){
+function marker_move_window(by_x, by_y, lines_offset){
+  if(by_y < 0){
+    let marker_y_render_min = line_position*(char_height*multiplier);
+    if(marker_y_render_min < render_y+(char_height*multiplier)*lines_offset) {
+      render_y -= (char_height*multiplier);
+    }
+  }
   let canvas_height = canvas.getBoundingClientRect().height;
-  let marker_y_render_max = (line_position+1)*char_height;
-  let marker_y_render_min = line_position*char_height;;
+  let marker_y_render_max = (line_position+1)*(char_height*multiplier);
   if(marker_y_render_max-render_y > canvas_height){
-     render_y += char_height
-  } else if(marker_y_render_min < render_y){
-     render_y -= char_height
+     render_y += (char_height*multiplier)
   }
 }
 
 function marker_drawings(){
-  marker_x = char_position*char_width;
-  marker_y = line_position*char_height;
+  marker_x = char_position*(char_width*multiplier);
+  marker_y = line_position*(char_height*multiplier);
   marker_y -= render_y;
   redraw();
   redraw_marker();
 }
 
 function redraw_marker(){
-  ctx.fillRect(marker_x-marker_offset_x, marker_y, 1, char_height);
+  if(!selection_enabled){
+    ctx.fillRect(marker_x-marker_offset_x, marker_y, 1, (char_height*multiplier));
+  }
 }
 
 setInterval(function(){
   redraw_marker();
   setTimeout(function(){
     ctx.fillStyle = field_bg;
-    ctx.fillRect(marker_x-marker_offset_x, marker_y, 1, char_height);
+    ctx.fillRect(marker_x-marker_offset_x, marker_y, 1, (char_height*multiplier));
     ctx.fillStyle = text_color;
   }, 1000);
 },2000);
@@ -247,34 +261,59 @@ let selection_enabled = false;
 canvas.addEventListener("mousedown", e => {
   canvasX = canvas.getBoundingClientRect().left;
   canvasY = canvas.getBoundingClientRect().top;
-  selection_start_line = Math.round((e.clientY-canvasY)/char_height)
-  selection_start_char = Math.round((e.clientX-canvasX)/char_width)
+  selection_start_line = Math.ceil((e.clientY-canvasY)/(char_height*multiplier))
+  selection_start_char = Math.round((e.clientX-canvasX)/(char_width*multiplier))
   if(selection_start_char > line_length[selection_start_line-1]){
     selection_start_char = line_length[selection_start_line-1]
   }
   selection_enabled = true
   redraw();
+  line_position = selection_start_line-1
+  char_position = selection_start_char
+  marker_move(0,0)
+  redraw_marker()
 });
 canvas.addEventListener("mouseup", e => {
   selection_enabled = false
+  line_position = selection_start_line-1
+  chars_selected = Math.round((e.clientX-canvasX)/(char_width*multiplier))-selection_start_char;
+  selection_end_char = selection_start_char+chars_selected;
+  if(selection_end_char > line_length[selection_start_line-1]){
+    selection_end_char = line_length[selection_start_line-1]
+  }
+  char_position = selection_end_char;
+  marker_x = char_position*(char_width*multiplier);
+  marker_y = line_position*(char_height*multiplier);
+  marker_y -= render_y;
 })
 canvas.addEventListener("mousemove", e => {
   if(selection_enabled){
     redraw();
     ctx.fillStyle = 'rgba(0,0,255,0.2)';
-    chars_selected = Math.round((e.clientX-canvasX)/char_width)-selection_start_char;
+    chars_selected = Math.round((e.clientX-canvasX)/(char_width*multiplier))-selection_start_char;
     max_selection = line_length[selection_start_line-1]-selection_start_char;
     if(chars_selected > max_selection){
       chars_selected = max_selection
     }
-    console.log(chars_selected)
-    ctx.fillRect((selection_start_char)*char_width,
-                 (selection_start_line-1)*char_height,
-                 chars_selected*char_width,
-                 char_height)
+    ctx.fillRect((selection_start_char)*(char_width*multiplier),
+                 (selection_start_line-1)*(char_height*multiplier),
+                 chars_selected*(char_width*multiplier),
+                 (char_height*multiplier))
     ctx.fillStyle = '#000';
   }
 })
+canvas.addEventListener("mousewheel", function(event){
+     marker_move(0, Math.round(event.deltaY/5))
+});
+
+function reset_window(){
+  char_position = 0;
+  line_position = 0;
+  current_line_length = line_length[0];
+  render_y = 0;
+  marker_offset_x = 0;
+  marker_move(0,0)
+}
 
 function cleanthismess(){
   text = "";
